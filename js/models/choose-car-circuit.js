234512797e9a4ed75e7f60a67d1a41a488b59979
Robert Cabrera <cabrera.rch@gmail.com>
2021-12-09 18:48:55 +0100

    Add choose curcuit panel and linked to button

--- js/models/choose-car-circuit.js
+++ js/models/choose-car-circuit.js
@@ -1,37 +1,82 @@
-let count = 1;
+/* CAR PANEL SELECTION */
 
-function renderCar () {
+let countCar = 1;
+
+function chooseCar () {
   const carContainer = document.getElementById('car');
-  carContainer.innerHTML = `<img class="my-car" src="/images/cars/car-${count}.png">`;
+  carContainer.innerHTML = `<img class="my-car" src="/images/cars/car-${countCar}.png">`;
 }
 
-const prevCar = document.getElementById('left-button');
-const nextCar = document.getElementById('right-button');
+const prevCar = document.getElementById('left-button-car');
+const nextCar = document.getElementById('right-button-car');
 
 prevCar.onclick = () => {
-  if (count > 1) {
-    count = count - 1;
-    renderCar();
+  if (countCar > 1) {
+    countCar = countCar - 1;
+    chooseCar();
   }
 };
 
 nextCar.onclick = () => {
-  if (count < 4) {
-    count = count + 1;
-    renderCar();
+  if (countCar < 4) {
+    countCar = countCar + 1;
+    chooseCar();
   }
 };
 
-renderCar();
+chooseCar();
 
 const carButton = document.getElementById('car-button');
-const closeButton = document.getElementById('close-car-selection')
+const closeCarButton = document.getElementById('close-car-selection')
 const carSelectionPanel = document.getElementById('car-selection');
 
 carButton.addEventListener('click', () => {
   carSelectionPanel.classList.add('display-panel');
 });
 
-closeButton.addEventListener('click', () => {
+closeCarButton.addEventListener('click', () => {
   carSelectionPanel.classList.remove('display-panel');
+});
+
+
+
+
+/* CIRCUIT PANEL SELECTION */
+let countCircuit = 1;
+
+function chooseCircuit () {
+  const circuitContainer = document.getElementById('circuit');
+  circuitContainer.innerHTML = `<img class="my-circuit" src="/images/circuits/circuit-${countCircuit}.png">`;
+}
+
+const prevCircuit = document.getElementById('left-button-circuit');
+const nextCircuit = document.getElementById('right-button-circuit');
+
+prevCircuit.onclick = () => {
+  if (countCircuit > 1) {
+    countCircuit = countCircuit - 1;
+    chooseCircuit();
+  }
+};
+
+nextCircuit.onclick = () => {
+  if (countCircuit < 3) {
+    countCircuit = countCircuit + 1;
+    chooseCircuit();
+  }
+};
+
+chooseCircuit();
+
+
+const raceButton = document.getElementById('race-button');
+const closeCircuitButton = document.getElementById('close-circuit-selection')
+const circuitSelectionPanel = document.getElementById('circuit-selection');
+
+raceButton.addEventListener('click', () => {
+  circuitSelectionPanel.classList.add('display-panel');
+});
+
+closeCircuitButton.addEventListener('click', () => {
+  circuitSelectionPanel.classList.remove('display-panel');
 });
